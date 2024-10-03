eaece32814fbc0ed6db672057c2e70ca30ab1787
moshams272 <mohamedshamseldeen95@gmail.com>
2024-10-03 07:13:07 +0300

    Fix error of file name change from landing to Landing while importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Landing from './Pages/landing/landing';
+import Landing from './Pages/Landing/Landing';
 const routes=createBrowserRouter([
   {path:"/",element:<Landing/>}
 ])
